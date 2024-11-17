eeff5619aa8ca4d0f424a6af89625a9bedecf1c3
NotAnsar <karrouach.ansar@gmail.com>
2024-11-17 01:28:14 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	// If you're not using API routes or server-side features:
+	// output: 'export',
+};
 
 export default nextConfig;
